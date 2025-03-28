9e2be1b2c3634fe1656f673a54d4443f697d645f
Abhish3k <im4bhishek10@gmail.com>
2025-03-28 19:39:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,28 @@ const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
 const counterLabel = document.getElementById("counterLabel");
+const multipleBtn = document.getElementById("multipleBtn");
 let count = 0;
+let multiplier = 1;
 
 increaseBtn.onclick = function() {
-    count++ ;
+    count+= multiplier;
     counterLabel.textContent = count;
 }
 
 decreaseBtn.onclick = function(){
-    count-- ;
+    count-= multiplier ;
     counterLabel.textContent = count;
 }
 
 resetBtn.onclick = function(){
-    count = 0
+    count = 0;
+    multiplier = 1;
+    multipleBtn.textContent = "1x"
     counterLabel.textContent = count;
+}
+
+multipleBtn.onclick = function(){
+    multiplier++;
+    multipleBtn.textContent = multiplier + "x" ;
 }
